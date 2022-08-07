724063adc8e56c7f5b1f5d7eda7af58d84400032
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-08-07 00:14:45 -0400

    pmpi: add MPI_Allreduce() test
    works, but not with MPI_IN_PLACE

--- perfetto_hpc/tests/test_mpi_fortran.F90
+++ perfetto_hpc/tests/test_mpi_fortran.F90
@@ -4,7 +4,17 @@ program main
     implicit none
 
     integer :: ierr
+    integer :: rank
+    integer :: sum
 
     call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+#if 1
+    call MPI_Allreduce(rank, sum, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr)
+#else
+    sum = rank
+    call MPI_Allreduce(MPI_IN_PLACE, sum, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr)
+#endif
+    if (rank == 0) print *, 'sum', sum
     call MPI_Finalize(ierr)
 end program
